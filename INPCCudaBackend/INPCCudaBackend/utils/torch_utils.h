67ed49ba27650cae51b15e516dafbe87c6747e13
flo <flo.hahlbohm@gmail.com>
2025-07-08 11:36:12 +0200

    Fix long re-compilation times

--- INPCCudaBackend/INPCCudaBackend/utils/torch_utils.h
+++ INPCCudaBackend/INPCCudaBackend/utils/torch_utils.h
@@ -1,6 +1,12 @@
 #pragma once
 
-#include <functional>
 #include <torch/extension.h>
+#include <functional>
 
-std::function<char*(size_t N)> resize_function_wrapper(torch::Tensor& t);
+inline std::function<char*(size_t N)> resize_function_wrapper(torch::Tensor& t) {
+    auto lambda = [&t](const size_t N) {
+        t.resize_({static_cast<long long>(N)});
+		return reinterpret_cast<char*>(t.contiguous().data_ptr());
+    };
+    return lambda;
+}
